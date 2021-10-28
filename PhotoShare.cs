af31a323fa04b9bfc600dad90283197ac777cafb
Claire du Preez <90896667+Clairedup@users.noreply.github.com>
2021-10-28 11:27:58 +0200

    Add files via upload
    PhotoShare Model Added

---
+++ PhotoShare.cs
@@ -0,0 +1,11 @@
+﻿namespace PhotoShareApp.Models
+{
+    public class PhotoShare
+    {
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        public string Image { get; set; }
+    }
+}
